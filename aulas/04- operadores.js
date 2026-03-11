13fe48c56da02ebcfabcf0f7e246b92118423b41
vitoriapeterlini <vitoria.peterlini@edu.senai.br>
2026-03-11 17:02:57 -0300

    Realizamos um sistema estilo banco e utilizamos diferentes contas e formulas

---
+++ aulas/04- operadores.js
@@ -0,0 +1,24 @@
+ //Operadores relacionais ou comparadores
+x = 22;
+y = 5;
+console.log("--------------------------------");
+console.log("x =", x, "| y =", y);
+console.log("x é igual a y?", x == y); //comparação de igualdade
+console.log("x é diferente de y?", x != y); //diferente de
+console.log("x maior que y?", x > y); // maior que
+console.log("x menor que y?", x < y); // menor que
+console.log("x maior ou igual a y?", x >= y); //maior igual que
+console.log("x menor ou igual a y?", x <= y); //menor igual que
+
+
+x = 5;
+
+
+console.log("--------------------------------");
+console.log("x =", x, "| y =", y);
+console.log("x é igual a y?", x == y); //comparação de igualdade
+console.log("x é diferente de y?", x != y); //diferente de
+console.log("x maior que y?", x > y); // maior que
+console.log("x menor que y?", x < y); // menor que
+console.log("x maior ou igual a y?", x >= y); //maior igual que
+console.log("x menor ou igual a y?", x <= y); //menor igual que
